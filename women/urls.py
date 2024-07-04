ebf792f2eb6c35d12d212a74d24215349b635db9
Vitali.Kondrus <dcti77@gmail.com>
2024-07-04 12:00:29 +0300

    added some simple/inclusion tags slug URL for Women and Category

--- women/urls.py
+++ women/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('addpage/', addpage, name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
+    path('post/<slug:post_slug>/', show_post, name='post'),
+    path('category/<slug:cat_slug>/', show_category, name='category'),
 ]
--- women/views.py
+++ women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 
 menu = [
@@ -11,8 +11,14 @@ menu = [
 
 
 def index(request): #Http request
-    posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+    posts = Women.objects.filter(is_published=True)
+    context = {'posts': posts,
+               'menu': menu,
+               'title': 'Главная страница',
+               'cat_selected': 0,
+               }
+
+    return render(request, 'women/index.html', context )
 
 
 def about(request): #Http request
@@ -29,6 +35,32 @@ def contact(request):
 
 def login(request):
     return HttpResponse("Войти")
+
+
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+    context = {'post': post,
+               'menu': menu,
+               'title': post.title,
+               'cat_selected': post.cat_id,
+               }
+
+    return render(request,'women/post.html', context=context)
+
+
+def show_category(request, cat_slug):
+    cat = Category.objects.filter(slug=cat_slug)
+    posts = Women.objects.filter(cat_id=cat[0].id)
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {'posts': posts,
+               'title': 'Отображение по рубкрикам',
+               'cat_selected': cat[0].id
+               }
+
+    return render(request, 'women/index.html', context)
 
 
 def pageNotFound(request, exception):
